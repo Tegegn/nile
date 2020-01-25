e8fefc306f35a6c906b954c1da66dc9b6fbfc880
Tegegn <tegegngelan@gmail.com>
2020-01-25 16:25:21 -0500

    add fake to fail test

--- src/test/java/com/purpose/nile/NileServiceApplicationTests.java
+++ src/test/java/com/purpose/nile/NileServiceApplicationTests.java
@@ -1,11 +1,16 @@
 package com.purpose.nile;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class NileServiceApplicationTests {
 
-
+    @Test
+    void testShouldFail(){
+        Assertions.fail();
+    }
 
 }
