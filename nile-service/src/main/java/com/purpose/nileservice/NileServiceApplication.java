ea71511903d1fa5eb227dc1f7649d55997fb3617
Tegegn <tegegngelan@gmail.com>
2020-01-25 13:32:18 -0500

    initial commit

---
+++ nile-service/src/main/java/com/purpose/nileservice/NileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.purpose.nileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NileServiceApplication.class, args);
+	}
+
+}
