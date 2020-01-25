4066b5cbd40d4840b2fa58f8edc21e263798ab97
Tegegn <tegegngelan@gmail.com>
2020-01-25 15:08:20 -0500

    add fake test

--- src/test/java/com/purpose/nile/NileServiceApplicationTests.java
+++ src/test/java/com/purpose/nile/NileServiceApplicationTests.java
@@ -1,11 +1,15 @@
 package com.purpose.nile;
 
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class NileServiceApplicationTests {
 
-
-
+    @Test
+    void test(){
+        Assumptions.assumeTrue(true);
+    }
 }
