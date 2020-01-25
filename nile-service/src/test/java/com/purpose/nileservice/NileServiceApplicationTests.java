2c0aff4d153876042f87ab3e7106c2cd83012f07
Tegegn <tegegngelan@gmail.com>
2020-01-25 14:25:47 -0500

    do not load context on test

--- nile-service/src/test/java/com/purpose/nileservice/NileServiceApplicationTests.java
+++ nile-service/src/test/java/com/purpose/nileservice/NileServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package com.purpose.nileservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class NileServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
